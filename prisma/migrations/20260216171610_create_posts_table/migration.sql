39c499ae8d9898db70200758def9b56c2959abf6
Josue <josuepico15@gmail.com>
2026-02-16 12:19:42 -0500

    feat: add Post model and database migration scripts
    - Create Post model with user relationship
    
    - Add database management scripts (migrate, generate, push, studio)
    
    - Install dotenv-cli for reliable environment variable handling
    
    - Remove redundant Prisma config files

---
+++ prisma/migrations/20260216171610_create_posts_table/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "pusblised" BOOLEAN NOT NULL DEFAULT false,
+    "publishedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_slug_key" ON "Post"("slug");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
